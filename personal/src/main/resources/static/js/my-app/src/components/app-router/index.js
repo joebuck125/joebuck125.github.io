7f1e30395f600f22a8b7f6a7e4789117b8c66824
joebuck125 <joe.jmoney@gmail.com>
2022-08-23 16:56:49 -0400

    need to finish importing preliminary pages and routing everything

--- personal/src/main/resources/static/js/my-app/src/components/app-router/index.js
+++ personal/src/main/resources/static/js/my-app/src/components/app-router/index.js
@@ -1,1 +1,21 @@
 import {Route, Switch} from 'react-router-dom';
+
+import ContactPage from '../../pages/Contact-page';
+import HomePage from '../../pages/Home-page';
+import JournalPage from '../../pages/Journal-page';
+import LoginPage from '../../pages/Login';
+
+const AppRouter = () => {
+    return (
+        <Switch>
+            <Route exact path={'/Home'} component={HomePage} />
+            <Route exact path={'/Journal-Page'} component={JournalPage} />
+            <Route exact path={'/Contact-Page'} component={ContactPage} />
+            <Route exact path={'/'} component={LoginPage} />
+        </Switch>
+
+    );
+    
+}
+
+export default AppRouter;
