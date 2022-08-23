64f405d2ab8fd404417b875a0f16bf3f9787d57b
joebuck125 <joe.jmoney@gmail.com>
2022-08-23 16:31:11 -0400

    journal copied over

--- personal/src/main/resources/static/js/my-app/src/App.js
+++ personal/src/main/resources/static/js/my-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Approuter from './components/app-router';
+import {BrowserRouter} from 'react-router-dom';
+import Footer from './components/footer';
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Header />
+      <Approuter />
+      <Footer />
+      </BrowserRouter> 
     </div>
   );
 }
